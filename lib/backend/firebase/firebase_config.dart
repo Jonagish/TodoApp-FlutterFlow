232334d5876776177cfae045129a4dbd6b957f2c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-11 00:01:04 +0000

    First push to Github

---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCCfMIqJ4ZVu_4fvcLDs4BLJhg9JMunTAU",
+            authDomain: "to-do-app-39g36f.firebaseapp.com",
+            projectId: "to-do-app-39g36f",
+            storageBucket: "to-do-app-39g36f.firebasestorage.app",
+            messagingSenderId: "784634549083",
+            appId: "1:784634549083:web:3fa076b174fc4d8bc8c07e"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
---
+++ lib/login/login_model.dart
@@ -0,0 +1,134 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'login_widget.dart' show LoginWidget;
+import 'package:flutter/material.dart';
+
+class LoginModel extends FlutterFlowModel<LoginWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final formKey2 = GlobalKey<FormState>();
+  final formKey1 = GlobalKey<FormState>();
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+
+  // State field(s) for SingUp-Email widget.
+  FocusNode? singUpEmailFocusNode;
+  TextEditingController? singUpEmailTextController;
+  String? Function(BuildContext, String?)? singUpEmailTextControllerValidator;
+  String? _singUpEmailTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Email... is required';
+    }
+
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'Has to be a valid email address.';
+    }
+    return null;
+  }
+
+  // State field(s) for SingUp-Password widget.
+  FocusNode? singUpPasswordFocusNode;
+  TextEditingController? singUpPasswordTextController;
+  late bool singUpPasswordVisibility;
+  String? Function(BuildContext, String?)?
+      singUpPasswordTextControllerValidator;
+  String? _singUpPasswordTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Password.. is required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  // State field(s) for ConfirmPass widget.
+  FocusNode? confirmPassFocusNode;
+  TextEditingController? confirmPassTextController;
+  late bool confirmPassVisibility;
+  String? Function(BuildContext, String?)? confirmPassTextControllerValidator;
+  String? _confirmPassTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Confirm Password... is required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  // State field(s) for LoginEmail widget.
+  FocusNode? loginEmailFocusNode;
+  TextEditingController? loginEmailTextController;
+  String? Function(BuildContext, String?)? loginEmailTextControllerValidator;
+  String? _loginEmailTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Email... is required';
+    }
+
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'Has to be a valid email address.';
+    }
+    return null;
+  }
+
+  // State field(s) for LoginPassword widget.
+  FocusNode? loginPasswordFocusNode;
+  TextEditingController? loginPasswordTextController;
+  late bool loginPasswordVisibility;
+  String? Function(BuildContext, String?)? loginPasswordTextControllerValidator;
+  String? _loginPasswordTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Password... is required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  @override
+  void initState(BuildContext context) {
+    singUpEmailTextControllerValidator = _singUpEmailTextControllerValidator;
+    singUpPasswordVisibility = false;
+    singUpPasswordTextControllerValidator =
+        _singUpPasswordTextControllerValidator;
+    confirmPassVisibility = false;
+    confirmPassTextControllerValidator = _confirmPassTextControllerValidator;
+    loginEmailTextControllerValidator = _loginEmailTextControllerValidator;
+    loginPasswordVisibility = false;
+    loginPasswordTextControllerValidator =
+        _loginPasswordTextControllerValidator;
+  }
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+    singUpEmailFocusNode?.dispose();
+    singUpEmailTextController?.dispose();
+
+    singUpPasswordFocusNode?.dispose();
+    singUpPasswordTextController?.dispose();
+
+    confirmPassFocusNode?.dispose();
+    confirmPassTextController?.dispose();
+
+    loginEmailFocusNode?.dispose();
+    loginEmailTextController?.dispose();
+
+    loginPasswordFocusNode?.dispose();
+    loginPasswordTextController?.dispose();
+  }
+}
